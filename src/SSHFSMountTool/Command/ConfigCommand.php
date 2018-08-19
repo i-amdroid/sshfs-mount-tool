0f7878a49a9e62279c6d7112afcf9e89ebf24696
Andrey Yurtaev <mdrdnt@gmail.com>
2018-08-19 16:49:12 +0300

    Finished user preferences handling. Optimized phar packaging.

--- src/SSHFSMountTool/Command/ConfigCommand.php
+++ src/SSHFSMountTool/Command/ConfigCommand.php
@@ -5,6 +5,7 @@ namespace SSHFSMountTool\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Process\Process;
 
 class ConfigCommand extends Command {
@@ -16,13 +17,22 @@ class ConfigCommand extends Command {
     $this->setAliases([
       'cfg',
     ]);
+    $this->setHelp('Open config file. By default used connection settings file.');
+    $this->addOption('settings', 's', InputOption::VALUE_NONE, 'Open SMT settings file (preferences)');
 
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
 
     global $preferences;
-    $config_file = get_config_file();
+
+    if ($input->getOption('settings')) {
+      $config_file = $preferences['user_preferences_file'];
+    }
+    else {
+      $config_file = get_config_file();
+    }
+
     $cmd = $preferences['editor'] . ' ' . $config_file;
 
     // Command execution
